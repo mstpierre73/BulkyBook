c38b8d2718628abfe84f8d16ef11c6a211530b40
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-15 16:41:34 -0500

    add icon library and create category page

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -15,5 +15,10 @@ namespace BulkyBookWeb.Controllers {
             IEnumerable<Category> objCategoryList = _db.Categories;
             return View(objCategoryList);
         }
+
+        //GET
+        public IActionResult Create() {
+            return View();
+        }
     }
 }
