cda2a725185760c0b8844961cc55714cdb426c4a
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-21 11:49:59 -0500

    Move files in new subproject

--- BulkyBook.Models/ErrorViewModel.cs
+++ BulkyBook.Models/ErrorViewModel.cs
@@ -1,5 +1,7 @@
-namespace BulkyBookWeb.Models {
-    public class ErrorViewModel {
+namespace BulkyBook.Models
+{
+    public class ErrorViewModel
+    {
         public string? RequestId { get; set; }
 
         public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
