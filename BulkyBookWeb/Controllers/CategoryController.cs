e680907addff784072e6de3d9db2d8d5fc53d617
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-21 13:56:55 -0500

    Add IRepository interface

--- BulkyBookWeb/Controllers/CategoryController.cs
+++ BulkyBookWeb/Controllers/CategoryController.cs
@@ -44,13 +44,13 @@ namespace BulkyBookWeb.Controllers {
                 return NotFound();
             }
             
-            var categoryFromDb = _db.Categories.Find(id);
+            var categoryFromDbFirst = _db.Categories.FirstOrDefault(u=>u.Name=="id");
 
-            if(categoryFromDb == null) {
+            if(categoryFromDbFirst == null) {
                 return NotFound();
             }
 
-            return View(categoryFromDb);
+            return View(categoryFromDbFirst);
         }
 
         //PUT
