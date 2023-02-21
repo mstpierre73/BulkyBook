f87c1b0bd4cef329a9b2c5e85cca62d75c462e81
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-21 15:29:05 -0500

    Add new CoverType model, add new table to db and add new interface to unitOfWork

--- BulkyBook.Models/CoverType.cs
+++ BulkyBook.Models/CoverType.cs
@@ -1,5 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -8,5 +10,11 @@ namespace BulkyBook.Models
 {
     public class CoverType
     {
+        [Key]
+        public int Id { get; set; }
+        [Display(Name = "Cover Type")]
+        [Required]
+        [MaxLength(50)]
+        public string Name { get; set; }
     }
 }
