044d35e42c87b031bf6629c55229a749989d452b
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-22 11:00:36 -0500

    Add table product to db and add IProduct interface

--- BulkyBook.DataAccess/Data/ApplicationDbContext.cs
+++ BulkyBook.DataAccess/Data/ApplicationDbContext.cs
@@ -9,5 +9,6 @@ namespace BulkyBook.DataAccess {
 
         public DbSet<Category> Categories { get; set; }
         public DbSet<CoverType> CoverTypes { get; set; }
+        public DbSet<Product> Products { get; set; }
     }
 }
