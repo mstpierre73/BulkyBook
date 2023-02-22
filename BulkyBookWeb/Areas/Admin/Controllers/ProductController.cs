98748d0425f91fadbd09b1e839c244e54e86dc81
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-22 14:49:53 -0500

    Add index and upsert pages for product

---
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -0,0 +1,95 @@
+﻿using BulkyBook.DataAccess;
+using BulkyBook.DataAccess.Repository.IRepository;
+using BulkyBook.Models;
+using Microsoft.AspNetCore.Mvc;
+
+namespace BulkyBookWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class ProductController : Controller
+    {
+
+        private readonly IUnitOfWork _unitOfWork;
+
+        public ProductController(IUnitOfWork unitOfWork)
+        {
+            _unitOfWork = unitOfWork;
+        }
+
+        public IActionResult Index()
+        {
+            IEnumerable<Product> objProductList = _unitOfWork.Product.GetAll();
+            return View(objProductList);
+        }
+
+       //GET
+        public IActionResult Upsert(int? id)
+        {
+            Product product = new();
+
+            if (id == null || id == 0)
+            {
+                //Create product
+                return View(product);
+            } 
+            else
+            {
+                //update product
+
+            }
+
+            return View(product);
+        }
+
+        //PUT
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Upsert(Product obj)
+        {
+            if (ModelState.IsValid)
+            {
+                _unitOfWork.Product.Update(obj);
+                _unitOfWork.Save();
+                TempData["success"] = "Product updated sucessfully";
+                return RedirectToAction("Index");
+            }
+
+            return View(obj);
+        }
+
+        //GET
+        public IActionResult Delete(int? id)
+        {
+            if (id == null || id == 0)
+            {
+                return NotFound();
+            }
+
+            var productFromDbFirst = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+
+            if (productFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+            return View(productFromDbFirst);
+        }
+
+        //DELETE
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public IActionResult DeletePOST(int? id)
+        {
+            var productFromDbFirst = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+            if (productFromDbFirst == null)
+            {
+                return NotFound();
+            }
+
+            _unitOfWork.Product.Remove(productFromDbFirst);
+            _unitOfWork.Save();
+            TempData["success"] = "Product deleted sucessfully";
+            return RedirectToAction("Index");
+        }
+    }
+}
