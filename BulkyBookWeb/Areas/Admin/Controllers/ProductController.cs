f01f7d19a0b2363656f531910ec1eec010afe479
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-24 15:02:00 -0500

    add delete product functionality

--- BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
+++ BulkyBookWeb/Areas/Admin/Controllers/ProductController.cs
@@ -56,8 +56,6 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                 return View(productViewModel);
 
             }
-
-            return View(productViewModel);
         }
 
         //PUT
@@ -74,6 +72,15 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                     var uploads = Path.Combine(wwwRootPath, @"images\products");
                     var extension = Path.GetExtension(file.FileName);
 
+                    if(obj.Product.ImageUrl != null)
+                    {
+                        var oldImagePath = Path.Combine(wwwRootPath, obj.Product.ImageUrl.TrimStart('\\'));
+                        if(System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
                     using (var fileStreams = new FileStream(Path.Combine(uploads, fileName + extension), FileMode.Create))
                     {
                         file.CopyTo(fileStreams);
@@ -81,7 +88,15 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
                     obj.Product.ImageUrl = @"\images\products\" + fileName + extension;
                 }
 
-                _unitOfWork.Product.Add(obj.Product);
+                if (obj.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(obj.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(obj.Product);
+                }
+                
                 _unitOfWork.Save();
                 TempData["success"] = "Product created sucessfully";
                 return RedirectToAction("Index");
@@ -90,47 +105,33 @@ namespace BulkyBookWeb.Areas.Admin.Controllers
             return View(obj);
         }
 
-        //GET
-        public IActionResult Delete(int? id)
-        {
-            if (id == null || id == 0)
-            {
-                return NotFound();
-            }
-
-            var productFromDbFirst = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
-
-            if (productFromDbFirst == null)
-            {
-                return NotFound();
-            }
-
-            return View(productFromDbFirst);
-        }
-
-        //DELETE
-        [HttpPost, ActionName("Delete")]
-        [ValidateAntiForgeryToken]
-        public IActionResult DeletePOST(int? id)
-        {
-            var productFromDbFirst = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
-            if (productFromDbFirst == null)
-            {
-                return NotFound();
-            }
-
-            _unitOfWork.Product.Remove(productFromDbFirst);
-            _unitOfWork.Save();
-            TempData["success"] = "Product deleted sucessfully";
-            return RedirectToAction("Index");
-        }
-
         #region API CALLS
         [HttpGet]
         public IActionResult GetAll()
         {
             var productList = _unitOfWork.Product.GetAll(includeProperties: "Category,CoverType");
             return Json(new { data = productList });
+        }
+
+        //DELETE
+        [HttpDelete]
+        public IActionResult Delete(int? id)
+        {
+            var productFromDbFirst = _unitOfWork.Product.GetFirstOrDefault(u => u.Id == id);
+            if (productFromDbFirst == null)
+            {
+                return Json(new { success = false, message = "Error while deleting" });
+            }
+
+            var oldImagePath = Path.Combine(_webHostEnvironment.WebRootPath, productFromDbFirst.ImageUrl.TrimStart('\\'));
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(productFromDbFirst);
+            _unitOfWork.Save();
+            return Json(new { success = true, message = "Product deleted successfully" });
         }
         #endregion
     }
--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -21,7 +21,7 @@ function loadDataTable() {
                 return `
                         <div class="btn-group" role="group">
                               <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-1 text-end" style="width: 125px"><i class="bi bi-pencil-square"></i>&nbsp; Edit</a>
-                              <a class="btn btn-danger mx-1 text-end" style="width: 125px"><i class="bi bi-trash-fill"></i>&nbsp; Delete</a>
+                              <a onClick=Delete('/Admin/Product/Delete/${data}') class="btn btn-danger mx-1 text-end" style="width: 125px"><i class="bi bi-trash-fill"></i>&nbsp; Delete</a>
                         </div>
                     `
                 }, 
@@ -29,5 +29,32 @@ function loadDataTable() {
             }
         ]
     });
+}
+
+function Delete(url) {
+    Swal.fire({
+        title: 'Are you sure?',
+        text: "You won't be able to revert this!",
+        icon: 'warning',
+        showCancelButton: true,
+        confirmButtonColor: '#3085d6',
+        cancelButtonColor: '#d33',
+        confirmButtonText: 'Yes, delete it!'
+    }).then((result) => {
+        if (result.isConfirmed) {
+            $.ajax({
+                url: url,
+                type: 'DELETE',
+                success: function (data) {
+                    if (data.success) {
+                        $("#productTable").DataTable().ajax.reload();
+                        toastr.success(data.message);
+                    } else {
+                        toastr.error(data.message);
+                    }
+                }
+            })
+        }
+    })
 }
 
