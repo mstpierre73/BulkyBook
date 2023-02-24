d2c22dfb1d8879f3ef5ce7094efe34d9e2d89014
Marise St-Pierre <naranjaqc@hotmail.com>
2023-02-24 09:16:00 -0500

    Populate productList table when api is called

--- BulkyBookWeb/wwwroot/js/product.js
+++ BulkyBookWeb/wwwroot/js/product.js
@@ -4,3 +4,30 @@ $(document).ready(function () {
     loadDataTable();
 })
 
+function loadDataTable() {
+    dataTable = $("#productTable").dataTable({
+        "ajax": {
+            "url":"/Admin/Product/GetAll"
+        },
+        "columns": [
+            { "data": "title", "width": "15%" },
+            { "data": "isbn", "width": "15%" },
+            { "data": "price", "width": "15%" },
+            { "data": "author", "width": "15%" },
+            { "data": "category.name", "width": "15%" },
+            {
+                "data": "id",
+                "render": function (data) {
+                return `
+                        <div class="btn-group" role="group">
+                              <a href="/Admin/Product/Upsert?id=${data}" class="btn btn-primary mx-1 text-end" style="width: 125px"><i class="bi bi-pencil-square"></i>&nbsp; Edit</a>
+                              <a class="btn btn-danger mx-1 text-end" style="width: 125px"><i class="bi bi-trash-fill"></i>&nbsp; Delete</a>
+                        </div>
+                    `
+                }, 
+                "width": "15%"
+            }
+        ]
+    });
+}
+
